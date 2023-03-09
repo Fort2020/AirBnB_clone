36b6a3e34923bf27dd012352f52619abf26ae471
fort2020 <frankfortdata@gmail.com>
2023-03-09 19:22:34 +0000

    Airbnb Tasks

---
+++ README.md
@@ -0,0 +1,1 @@
+This is This is 0x00. AirBnB Clone - The Console README.md
---
+++ models/user.py
@@ -0,0 +1,14 @@
+user.py
+-------
+#!/usr/bin/python3
+"""This module creates a User class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Class for managing user objects"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
